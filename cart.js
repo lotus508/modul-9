f961ef3210dd263c104b1be75b9fcacc245ce3a1
Жас <zhasulan@bk.ru>
2022-04-03 15:01:02 +0600

    корзина

--- cart.js
+++ cart.js
@@ -1,40 +1,44 @@
 'use strict';
 //Создайте объект cart — корзина:
-//var button = document.querySelector('button');
-//const product = prompt('Наименование товара');
-//button.onclick = function() {
-  //const product = prompt('Наименование товара');
-//console.log('\"' + product + '\"');
   const cart = {
-  tovar: "Кукла",
-  cena: 400,
-};
+    items: [],      //пустой массив - это товары
+    totalPrice: 0,  //- общая стоимость корзины
+    count: 0,       //- количество товаров
 
-console.log('cart: ', cart)
-//alert(cart);
+    getTotalPrice() { 
+      return this.totalPrice;
+    },
+    add(ProductName, ProductPrice, ProductCount = 1) {
+      const Product = {
+        'Продукт': ProductName,
+        ProductPrice: ProductPrice,
+        'ProductCount': ProductCount,
+      };
+      this.items.push(Product);
+      this.increaseCount(ProductCount);
+      this.calculateItemPrice();
+    },
+    increaseCount(ProductCount) {
+      this.count += ProductCount;
+    },
+    calculateItemPrice() {
+        this.totalPrice = this.items.reduce(
+        (total, item) => total + item.ProductPrice * item.ProductCount, 0
+      );
+    },
+    clear() {
+      this.items = [];
+      this.totalPrice = 0;
+      this.count = 0;
+    },
+    print() {
+      console.log(`${JSON.stringify(this.items)}\n стоимость корзины: ${this.totalPrice}`);
+    },
+  };
 
-cart.tovar="Машинка";
-cart.cena=950;
-
-console.log('cart: ', cart)
-//alert(cart);
-
-let user = {};
-console.log('useruser: ', user);
-user.name = "John";
-user.surname = "Smith";
-user.name = "Pete";
-console.log('useruser: ', user)
-
-const person = {
-  name: ['Bob', 'Smith'],
-  age: 32,
-  gender: 'male',
-  interests: ['music', 'skiing'],
-  bio: function() {
-    alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
-  },
-  greeting: function() {
-    alert('Hi! I\'m ' + this.name[0] + '.');
-  }
-};
+    cart.add("Лук", 720, 1);
+    cart.add("Картофель", 320, 7);
+    cart.add("Апельсин", 880, 2);
+    cart.add("Курица", 2200, 3);
+    cart.add("Сахар", 375, 4);
+    cart.print();
